dca5ac8414008321f0c7985301d53481e310cd80
marjan <74914575+Marjan-Binta-Aziz@users.noreply.github.com>
2022-05-26 03:33:33 +0600

    updated

--- src/Pages/Purchases/Purchase.js
+++ src/Pages/Purchases/Purchase.js
@@ -8,7 +8,7 @@ import Loading from "../Shared/Loading";
 const Purchase = () => {
   const [disable, setDisable] = useState(false);
   const [authUser] = useAuthState(auth);
-  const [quantity, setQuantity] = useState(null);
+  const [quantity, setQuantity] = useState(0);
   const [loading, setLoading] = useState(false);
   const { toolsId } = useParams();
   const navigate = useNavigate();
@@ -84,52 +84,43 @@ const Purchase = () => {
   return (
     <div className="pt-5 justify-center px-5 mx-32 min-h-screen">
         <h2 className=" text-center text-primary text-4xl mb-5 uppercase">
-          Purchase here
+          Purchase
         </h2>
-      <div className="flex flex-row-reverse lg:col-span-2 sm:col-span-1">
+      <div className="flex flex-row-reverse gap-5 lg:col-span-2 sm:col-span-1">
         <div className="card w-fit h-fit bg-base-100 shadow-xl">
           <div className="card-body">
             <h2 className=" text-2xl font-bold">Name: {tools.name}</h2>
             <p>Description: {tools.description}</p>
             <p>Minimum_Order_quantity: {tools.min_ord_quantity}</p>
             <p>Available_Quantity: {tools.quantity}</p>
-            <p>Price_Per_piece: {tools.price}</p>
-            <p>Your Total Price: {parseInt(quantity) * parseInt(tools.price)}</p>
+            <p>Price: {tools.price}</p>
+            <p className="text-red-600 text-xl">Your Total Price: {parseInt(quantity) * parseInt(tools.price)}</p>
           </div>
         </div>
         <div className="w-full max-w-lg mx-auto">
           <h2 className=" text-3xl text-primary text-center mt-10 mb-0">
-            Provide Your Information
+            Fill this Form Carefully!
           </h2>
           <form onSubmit={handleForm}>
             <div className="form-control w-full max-w-lg">
-              <label className="label">
-                <span className="label-text">Name:</span>
-              </label>
               <input
                 required
                 type="text"
                 value={authUser?.displayName}
                 disabled
-                className="input input-bordered input-primary w-full max-w-lg input-sm"
+                className="input input-bordered input-primary w-full max-w-lg input-sm my-3"
               />
             </div>
             <div className="form-control w-full max-w-lg mt-1">
-              <label className="label">
-                <span className="label-text">Email:</span>
-              </label>
               <input
                 required
                 type="email"
                 value={authUser?.email}
                 disabled
-                className="input input-bordered input-primary w-full max-w-lg input-sm"
+                className="input input-bordered input-primary w-full max-w-lg input-sm my-3"
               />
             </div>
             <div className="form-control w-full max-w-lg mt-1">
-              <label className="label">
-                <span className="label-text">Quantity:</span>
-              </label>
               <input
                 required
                 type="number"
@@ -140,30 +131,24 @@ const Purchase = () => {
                 }}
                 defaultChecked={parseInt(tools.min_ord_quantity)}
                 placeholder={`min Quantity: ${tools.min_ord_quantity}`}
-                className="input input-bordered input-primary w-full max-w-lg input-sm"
+                className="input input-bordered input-error w-full max-w-lg input-sm my-3"
               />
             </div>
             <div className="form-control w-full max-w-lg mt-1">
-              <label className="label">
-                <span className="label-text">Mobile:</span>
-              </label>
               <input
                 required
                 type="text"
                 name="mobile"
-                placeholder="+880"
-                className="input input-bordered input-primary w-full max-w-lg input-sm"
+                placeholder="Phone"
+                className="input input-bordered input-primary w-full max-w-lg input-sm my-3"
               />
             </div>
             <div className="form-control w-full max-w-lg mt-1">
-              <label className="label">
-                <span className="label-text">Address:</span>
-              </label>
               <textarea
                 required
                 name="address"
-                className="textarea input-sm textarea-primary"
-                placeholder=""
+                className="textarea input-sm my-3 textarea-primary"
+                placeholder="Address"
               ></textarea>
             </div>
             <button
