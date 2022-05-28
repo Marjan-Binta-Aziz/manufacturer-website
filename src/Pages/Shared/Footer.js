cafafa753da67f7840335cb95c9952800f5f57b7
marjan <74914575+Marjan-Binta-Aziz@users.noreply.github.com>
2022-05-28 14:30:02 +0600

    Footer

--- src/Pages/Shared/Footer.js
+++ src/Pages/Shared/Footer.js
@@ -1,90 +1,53 @@
+import { faBrush, faClock, faLocation } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-
 const Footer = () => {
+    const today = new Date();
+    const year = today.getFullYear();
     return (
         <div>
-             <section class="info_section layout_padding">
-    <div class="container">
-      <div class="info_contact">
-        <div class="row">
-          <div class="col-md-4">
-            <a href="">
-              <img src="images/location-white.png" alt=""/>
-              <span>
-                Passages of Lorem Ipsum available
-              </span>
-            </a>
-          </div>
-          <div class="col-md-4">
-            <a href="">
-              <img src="images/telephone-white.png" alt=""/>
-              <span>
-                Call : +012334567890
-              </span>
-            </a>
-          </div>
-          <div class="col-md-4">
-            <a href="">
-              <img src="images/envelope-white.png" alt=""/>
-              <span>
-                demo@gmail.com
-              </span>
-            </a>
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-md-8 col-lg-9">
-          <div class="info_form">
-            <form action="">
-              <input type="text" placeholder="Enter your email"/>
-              <button>
-                subscribe
-              </button>
-            </form>
-          </div>
-        </div>
-        <div class="col-md-4 col-lg-3">
-          <div class="info_social">
-            <div>
-              <a href="">
-                <img src="images/fb.png" alt=""/>
-              </a>
-            </div>
-            <div>
-              <a href="">
-                <img src="images/twitter.png" alt=""/>
-              </a>
-            </div>
-            <div>
-              <a href="">
-                <img src="images/linkedin.png" alt=""/>
-              </a>
-            </div>
-            <div>
-              <a href="">
-                <img src="images/instagram.png" alt=""/>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-
+                <footer class="footer p-10 bg-secondary text-base-content">
+                <div className="flex ">
+                        <a className="w-32" href="/home"><img src="https://i.ibb.co/ncrnV11/logo.png" alt="#"/></a>
+                    </div>
+    <div>
+        <span class="footer-title">Our Partner</span> 
+        <a class="link link-hover"><FontAwesomeIcon className='text-primary' icon={faBrush} /> Chinabd Shop</a> 
+        <a class="link link-hover"><FontAwesomeIcon className='text-primary' icon={faBrush} /> Imported from US/China</a> 
+        <a class="link link-hover"><FontAwesomeIcon className='text-primary' icon={faBrush} /> britannica.com</a> 
+        <a class="link link-hover"><FontAwesomeIcon className='text-primary' icon={faBrush} /> Norsendi Fruits</a>
+    </div> 
+    <div>
+        <span class="footer-title">Opening time</span> 
+        <p ><FontAwesomeIcon className='text-primary' icon={faClock} /> We Are Available 24/7 in Online</p>
+        
+        
+    </div> 
+    <div>
+    <span class="footer-title">Shop From Place</span>
+        <a class="link link-hover"><FontAwesomeIcon className='text-primary' icon={faLocation} /> Dhaka</a>
+        <a class="link link-hover"><FontAwesomeIcon className='text-primary' icon={faLocation} />  CHittagong</a>
+        <a class="link link-hover"><FontAwesomeIcon className='text-primary' icon={faLocation} />  Newurk</a>
+        
     </div>
-  </section>
-
-  <footer class="container-fluid footer_section">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-7 col-md-9 mx-auto">
-          <p>
-            &copy; 2019 All Rights Reserved By
-            <a href="https://html.design/">Free Html Templates</a>
-          </p>
+    </footer> 
+    <footer class="footer px-10 py-4 border-t bg-accent text-base-content border-base-300 ">
+    <div class="items-center grid-flow-col text-white">
+    <a className="w-10" href="/home"><img src="https://i.ibb.co/ncrnV11/logo.png" alt="#"/></a>
+        <p>Brush to Rush <br /> Providing reliable tech since 1992</p>
+    </div> 
+    <div class="md:place-self-center md:justify-self-end">
+        <div class="grid grid-flow-col gap-4">
+        <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></a> 
+        <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a>
+        <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
         </div>
-      </div>
     </div>
-  </footer>
+    </footer>
+    <div className="col-md-12">
+        <p>Copyright &copy; {year} <a href="https://github.com/Marjan-Binta-Aziz"> Marjan Binta Aziz</a></p>
+    </div>
+                
     </div>
     );
 };
